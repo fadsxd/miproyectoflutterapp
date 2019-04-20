667214e0434a5de0f47c5b665ad17cd0144a22b8
fadsxd <mickyfads5@gmail.com>
2019-04-20 14:50:05 -0400

    arreglando todo el codigo

--- lib/main.dart
+++ lib/main.dart
@@ -2,148 +2,140 @@ import 'package:flutter/material.dart';
 import 'package:audioplayers/audio_cache.dart';
 import 'package:audioplayers/audioplayers.dart';
 
-
 void main() => runApp(MaterialApp(
-  home: MusicPlayer(),
-  theme: ThemeData.dark(),
-  ));
+      theme: ThemeData.dark(),
+      home: MusicPlayer(),
+    ));
 
-  class MusicPlayer extends StatefulWidget{
-    @override
-    _MusicPlayerState createState() => _MusicPlayerState();
-  }
+class MusicPlayer extends StatefulWidget {
+  @override
+  _MusicPlayerState createState() => _MusicPlayerState();
+}
 
-  class _MusicPlayerState extends State <MusicPlayer>{
-    final audioName = "audio1.mp3";
-    final musicName = "Octavia - Te Quiero mucho";
-    final imageURL = "https://los40es00.epimg.net/los40/imagenes/2017/11/28/album/1511885438_220718_1512040981_album_normal.jpg";
-    
-    Duration duration = Duration();
-    Duration position = Duration();
+class _MusicPlayerState extends State<MusicPlayer> {
+  final audioName = "audio1.mp3";
+  final musicName = "Octavia-Te Quiero Mucho";
+  final imageURL = "http://4.bp.blogspot.com/-x56AMzhJ7YM/UYlzz3U2c7I/AAAAAAAAl1o/jt3niRsbvac/s1600/octavia.jpg";
 
-    AudioPlayer audioPlayer;
-    AudioCache audioCache;
+  AudioPlayer audioPlayer;
+  AudioCache audioCache;
 
-    double volumen = 1;
-    double currentVolume = 0;
+  double volume = 1;
+  double currentVolume;
 
-    @override
-      void initState(){
-        
-        super.initState();
+  Duration duration = Duration();
+  Duration position = Duration();
 
-        audioPlayer = AudioPlayer();
-        audioCache = AudioCache(fixedPlayer: audioPlayer);
+  @override
+  void initState() {
+    super.initState();
 
-        audioPlayer.positionHandler = (p) => setState((){
+    audioPlayer = AudioPlayer();
+    audioCache = AudioCache(fixedPlayer: audioPlayer);
+
+    audioPlayer.positionHandler = (p) => setState(() {
           position = p;
         });
-        audioPlayer.durationHandler = (p) => setState((){
-          duration = p;
-        });
-      }
 
-    Widget build(BuildContext context){
-      return Scaffold(
-        appBar: AppBar(
-          title: Text("Music Player", style: TextStyle(
-          fontSize: 20,
-          color: Colors.yellow,
-        )),
+    audioPlayer.durationHandler = (d) => setState(() {
+          duration = d;
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Music Player",
+            style: TextStyle(fontSize: 20, color: Colors.yellow)),
+        centerTitle: true,
       ),
       body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-        
+          child: Padding(
+        padding: const EdgeInsets.all(8.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Text(musicName, style: TextStyle(fontSize: 20)),
             Padding(
-              padding: const EdgeInsets.only(bottom: 10),
-              child: Text(musicName, style: TextStyle(fontSize: 20)),
+              padding: const EdgeInsets.only(top: 12, bottom: 12),
+              child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.network(imageURL)),
             ),
-            Image.network(imageURL),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-          children: audioControls(),
+            Card(
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(top: 15),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: audioControls(),
+                    ),
+                  ),
+                  Slider(
+                    activeColor: Colors.purpleAccent,
+                    value: position.inSeconds.toDouble(),
+                    max: duration.inSeconds.toDouble(),
+                    onChanged: (double seconds) {
+                      setState(() {
+                        audioPlayer.seek(Duration(seconds: seconds.toInt()));
+                      });
+                    },
+                  )
+                ],
+              ),
             ),
-            Slider(
-            activeColor: Colors.green,
-              value: position.inSeconds.toDouble(),
-              max: duration.inSeconds.toDouble(),
-              onChanged: (double seconds){
-              setState(() {
-                              audioPlayer.seek(Duration(seconds: seconds.toInt()));
-                            });
-              },
-            )
           ],
-        ))),
-      );
-    }
-    Widget CreateIconButton(IconData icon,Color color, VoidCallback onPressed){
-      return IconButton(
-         icon: Icon(icon),
-         iconSize: 30,
-         color: color,
-         onPressed: onPressed,
-        );
-    }
-
-    List<Widget> audioControls(){
-      return <Widget>[
-          CreateIconButton(Icons.play_arrow,Colors.grey,()
-        {
-            audioCache.play(audioName);   
-        }),
-        CreateIconButton(Icons.pause,Colors.green,()
-        {
-          audioPlayer.pause();
-        }),
-        CreateIconButton(Icons.stop,Colors.purple,()
-        {
-          audioPlayer.stop();
-        }),
-        CreateIconButton(Icons.volume_up,Colors.orange,()
-        {
-          if(volumen < 1)
-          {
-            volumen += 0.1;
-          audioPlayer.setVolume(volumen);
-          }
-          
-        }),
-        CreateIconButton(Icons.volume_down,Colors.orange,()
-        {
-          if(volumen != 0)
-          {
-            volumen -= 0.1;
-          audioPlayer.setVolume(volumen);
-          }
-        }),
-        CreateIconButton(Icons.volume_off,Colors.red,()
-        {
-          if(volumen != 0){
-            currentVolume = volumen;
-            volumen = 0;
-          }else{
-            volumen = currentVolume;
-          }
-          audioPlayer.setVolume(volumen);
-        }),
-        CreateIconButton(Icons.skip_next,Colors.black, ()
-        {
-          for (var i = 0; i < 3; i++) {
-            audioPlayer.play(audioName);
-          }
-        })
-      ];
-    }
+        ),
+      )),
+    );
   }
 
+  Widget CreateIconButton(IconData icon, Color color, VoidCallback onPressed) {
+    return IconButton(
+      icon: Icon(icon),
+      iconSize: 30,
+      color: color,
+      onPressed: onPressed,
+    );
+  }
 
+  List<Widget> audioControls() {
+    return <Widget>[
+      CreateIconButton(Icons.skip_previous, Colors.black, (){
 
-  
-
-
-  
+      }),
+      CreateIconButton(Icons.play_arrow, Colors.green, () {
+        audioCache.play(audioName);
+      }),
+      CreateIconButton(Icons.pause, Colors.grey, () {
+        audioPlayer.stop();
+      }),
+      CreateIconButton(Icons.volume_up, Colors.green, () {
+        if (volume < 1) {
+          volume += 0.1;
+          audioPlayer.setVolume(volume);
+        }
+      }),
+      CreateIconButton(Icons.volume_down, Colors.red, () {
+        if (volume > 0) {
+          volume -= 0.1;
+          audioPlayer.setVolume(volume);
+        }
+      }),
+      CreateIconButton(Icons.volume_off, Colors.red, () {
+        if (volume != 0) {
+          currentVolume = volume;
+          volume = 0;
+        } else {
+          volume = currentVolume;
+        }
+        audioPlayer.setVolume(volume);
+      }),
+      CreateIconButton(Icons.skip_next, Colors.black, (){
+      
+      })
+    ];
+  }
+}
